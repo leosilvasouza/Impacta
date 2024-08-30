aa3d1ea6c99ccb8c75a19d9402c4aecdb582b453
Leonardo Souza <leonardosilva.souza@yahoo.com.br>
2024-08-29 22:55:57 -0300

    Update hello-server.go

--- hello-server.go
+++ hello-server.go
@@ -1,20 +1,13 @@
 package main
-
 import (
-	"fmt"
-	"log"
-	"net/http"
+    "fmt"
+    "net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
-}
-
 func main() {
-	http.HandleFunc("/", helloHandler)
-
-	fmt.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatalf("Could not start server: %s\n", err)
-	}
+    http.HandleFunc("/", HelloServer)
+    http.ListenAndServe(":13000", nil)
+}
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
